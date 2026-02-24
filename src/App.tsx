2ba101e9c6f56f6e2fe6920e291bc1fb15229b1e
serhat-kocyigit <seroky.0438@gmail.com>
2026-02-24 21:31:51 +0300

    Initial project setup with Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+ function App() {
+ return (
+ <div>
+ <h1>Web Tasarimi ve Programlama </h1>
+ <h2>LAB -1</h2>
+ <p>Ad Soyad: Serhat Koçyiğit</p>
+ <p>Ogrenci No: 230541064</p>
+ </div>
+)
+10 }
+
+export default App
